75cc17bab4a62dcf12210806eee38b9921333773
psyhello <nar0gind@gmail.com>
2020-01-17 18:00:40 +0300

    Create index.php
    Done.

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * 
+ */
+$apiTemplateSet1=[
+		"/api/items/%id%/%name%",
+		"/api/items/%id%/%role%",
+		"/api/items/%id%/%salary%"];
+class User
+{
+	public $name ;
+	public $id ;
+	public $role ;
+	public $salary ;
+
+
+
+	function __construct($name,$id,$role,$salary)
+	{
+		$this->name = $name;
+		$this->id = $id;
+		$this->role = $role;
+		$this->salary = $salary;
+	}
+
+
+
+	public function getApiPath($user,$apiTemplateSet1)
+	{
+	 	$repl = array('%id%','%name%','%role%','%salary%' );
+
+	 	$userInfo = array($user->id, $user->name, $user->role, $user->salary);
+
+		$apiTemplate = str_replace($repl, $userInfo, $apiTemplateSet1);
+	 	
+ 
+	  var_dump($apiTemplate);
+	}
+
+}
+
+$user = new User('John Dow','20','QA','100');
+
+$user->getApiPath($user,$apiTemplateSet1);
+
